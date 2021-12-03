cad996df5b3ced7891e9604a4e7f9000f536629a
Ben Wingerter <benwingerter01@gmail.com>
2021-12-03 08:18:29 -0600

    Remove Bee logger

--- src/BeeSimulation/agents/Bee.java
+++ src/BeeSimulation/agents/Bee.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
-import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
